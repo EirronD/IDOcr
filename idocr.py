19137a989c87ecd7a17ff50efd16ea3d7e87d753
EirronD <dagleeirron27@gmail.com>
2025-03-05 22:09:30 +0800

    os

--- idocr.py
+++ idocr.py
@@ -4,6 +4,7 @@ import numpy as np
 from flask import Flask, request, jsonify
 from PIL import Image
 import pytesseract
+import os
 
 app = Flask(__name__)
 
@@ -55,4 +56,6 @@ def extract_text():
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
