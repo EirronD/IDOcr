3cc8d9bd0367b32fd49a4532c0dc7470c32acf28
EirronD <dagleeirron27@gmail.com>
2025-03-05 22:15:25 +0800

    8080

--- idocr.py
+++ idocr.py
@@ -56,6 +56,6 @@ def extract_text():
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
